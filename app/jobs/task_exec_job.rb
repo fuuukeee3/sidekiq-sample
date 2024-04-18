6feac7838555adb781eb4fe97571b016c66d8c03
fu <semarusika@gmail.com>
2024-04-18 23:18:25 +0900

    jobサンプル

--- app/jobs/task_exec_job.rb
+++ app/jobs/task_exec_job.rb
@@ -3,6 +3,7 @@ class TaskExecJob < ApplicationJob
 
   def perform(**args)
     task = args[:task]
+    sleep 5
     puts "#{ Time.zone.now.strftime('%Y/%m/%d-%H:%M:%S')} #{task.content}を実行しました。"
   end
 end
